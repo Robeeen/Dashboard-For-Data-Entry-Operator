8e33aebac2e5b8dab75acd43fea9ad141c56cc4e
Robeeen <robeeen@gmail.com>
2024-09-30 15:54:50 +0600

    creating admin panel for Data Entry Dashboard

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -16,4 +16,21 @@ add_action( 'admin_menu', 'dashboard_admin_menu' );
 function my_admin_page_contents(){
 
     echo __("hellow this is admin");
+    global $wpdb;
+    $custom_table = $wpdb->prefix . 'users';    
+    $query = $wpdb->get_results("SELECT * FROM $custom_table");
+    echo "<table border=1>";
+            echo "<tbody>";
+            echo "<tr>";
+            echo "<th>User Name </th><th>Company Name </th><th>Controls</th>";
+            echo "</tr>";
+    if($query){        
+        foreach($query as $display){            
+            echo "<tr><td>$display->user_login</td><td>$display->user_email</td><td>" . "<input type='checkbox'>" . "</td></tr>"; 
+        
+        }}
+        echo "</tbody>";
+        echo "</table>";
+
+
 }
